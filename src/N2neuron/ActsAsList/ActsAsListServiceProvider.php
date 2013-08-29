191f6e94709cdddeaf0a13507b2688b145c79e20
Eric Perkins <e.perkins@n2neuron.com>
2013-08-29 14:23:00 -0500

    Commiting initial empty Laravel package structure.

---
+++ src/N2neuron/ActsAsList/ActsAsListServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace N2neuron\ActsAsList;
+
+use Illuminate\Support\ServiceProvider;
+
+class ActsAsListServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('n2neuron/acts-as-list');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
